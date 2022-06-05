7524f8300b6f04f48c710ad72f139d0345656f8e
Amish Verma <amishverma1998@gmail.com>
2022-06-05 18:14:09 +0530

    React strap added again

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./App.css";
 
 import Icon from "./components/Icon";
+// *************** React-toastify***************
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// **************** Reactstrap*****************
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const itemArray = new Array(9).fill("empty");
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
       <Icon />
+      <Card>Hello</Card>
     </div>
   );
 }
