8c7e9da9b20326700afeedc8ee84b860bccf06cc
Amish Verma <amishverma1998@gmail.com>
2022-06-05 11:50:52 +0530

    Background color changed and icosn are rendered from react-icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
+
+import Icon from "./components/Icon";
+
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Icon />
     </div>
   );
 }
