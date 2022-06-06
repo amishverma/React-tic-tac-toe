546dbc4b81044149f290498f8ff2e6d026fabd80
Amish Verma <amishverma1998@gmail.com>
2022-06-06 16:06:04 +0530

    Displayed the play area

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 
 import Icon from "./components/Icon";
 // *************** React-toastify***************
@@ -9,15 +8,53 @@ import "react-toastify/dist/ReactToastify.css";
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "./App.css";
+import { type } from "@testing-library/user-event/dist/type";
+
 const itemArray = new Array(9).fill("empty");
 
 function App() {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
+  };
+
+  const checkIsWinner = () => {};
+
+  const changeItem = (itemNumber) => {
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
+    }
+    if (itemArray[itemNumber] === "empty") {
+      itemArray = isCross ? "cross" : "cirlce";
+      setIsCross(!isCross);
+    } else {
+      return toast("Aready fille", { type: "eroor" });
+
+      checkIsWinner();
+    }
+  };
   return (
-    <div>
-      <h1>Hello world</h1>
-      <Icon />
-      <Card>Hello</Card>
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
